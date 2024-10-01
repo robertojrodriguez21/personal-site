f72e1cc693abb1a66b151ada6402d88785bea133
Roberto J Rodriguez <robertojrodriguez21@gmail.com>
2024-09-30 21:50:28 -0500

    Created addCommentDislike call to usersRoute.

--- backend/routes/usersRoute.js
+++ backend/routes/usersRoute.js
@@ -221,6 +221,26 @@ router.put("/removeCommentLike/:user_id/:comment_id", async (request, response) 
 });
 
 
+// Update One - add one comment dislike to one user
+router.put("/addCommentDislike/:user_id/:comment_id", async (request, response) => {
+  try {
+    const { user_id, comment_id } = request.params;
+
+    const result = await User.findByIdAndUpdate(user_id, { $addToSet: { commentDislikes: comment_id, dislikes: comment_id } });
+
+    if (!result) {
+      return response.status(404).json({ message: "User not found" });
+    }
+
+    return response
+      .status(200)
+      .send({ message: "User updated successfully" });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
 // DELETE
 // Delete One
 router.delete("/:user_id", async (request, response) => {
