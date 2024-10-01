fdebc43d8886a77b0af85569caa6b0f5849818d6
Roberto J Rodriguez <robertojrodriguez21@gmail.com>
2024-09-30 21:24:36 -0500

    Created removeCommentLike call to usersRoute.

--- backend/routes/usersRoute.js
+++ backend/routes/usersRoute.js
@@ -200,6 +200,27 @@ router.put("/addCommentLike/:user_id/:comment_id", async (request, response) => 
   }
 });
 
+// Update One - remove one comment like from one user
+router.put("/removeCommentLike/:user_id/:comment_id", async (request, response) => {
+  try {
+    const { user_id, comment_id } = request.params;
+
+    const result = await User.findByIdAndUpdate(user_id, { $pull: { commentLikes: comment_id, likes: comment_id } });
+
+    if (!result) {
+      return response.status(404).json({ message: "User not found" });
+    }
+
+    return response
+      .status(200)
+      .send({ message: "User updated successfully" });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+
 // DELETE
 // Delete One
 router.delete("/:user_id", async (request, response) => {
