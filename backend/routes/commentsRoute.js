aab9f1c076ca23a8924bba68b47e0702c9787ba2
Roberto J Rodriguez <robertojrodriguez21@gmail.com>
2024-10-01 00:14:37 -0500

    Created addLike, removeLike, addDislike, removeDislike calls to commentsRoute.

--- backend/routes/commentsRoute.js
+++ backend/routes/commentsRoute.js
@@ -95,6 +95,86 @@ router.put("/updateOne/:comment_id", async (request, response) => {
   }
 });
 
+// Update One - add one like to one comment
+router.put("/addLike/:comment_id/:user_id", async (request, response) => {
+  try {
+    const { comment_id, user_id } = request.params;
+
+    const result = await Comment.findByIdAndUpdate(comment_id, { $addToSet: { likes: user_id } });
+
+    if (!result) {
+      return response.status(404).json({ message: "Comment not found" });
+    }
+
+    return response
+      .status(200)
+      .send({ message: "Comment updated successfully" });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+// Update One - remove one like from one comment
+router.put("/removeLike/:comment_id/:user_id", async (request, response) => {
+  try {
+    const { comment_id, user_id } = request.params;
+
+    const result = await Comment.findByIdAndUpdate(comment_id, { $pull: { likes: user_id } });
+
+    if (!result) {
+      return response.status(404).json({ message: "Comment not found" });
+    }
+
+    return response
+      .status(200)
+      .send({ message: "Comment updated successfully" });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+// Update One - add one dislike to one comment
+router.put("/addDislike/:comment_id/:user_id", async (request, response) => {
+  try {
+    const { comment_id, user_id } = request.params;
+
+    const result = await Comment.findByIdAndUpdate(comment_id, { $addToSet: { dislikes: user_id } });
+
+    if (!result) {
+      return response.status(404).json({ message: "Comment not found" });
+    }
+
+    return response
+      .status(200)
+      .send({ message: "Comment updated successfully" });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+// Update One - remove one dislike from one comment
+router.put("/removeDislike/:comment_id/:user_id", async (request, response) => {
+  try {
+    const { comment_id, user_id } = request.params;
+
+    const result = await Comment.findByIdAndUpdate(comment_id, { $pull: { dislikes: user_id } });
+
+    if (!result) {
+      return response.status(404).json({ message: "Post not found" });
+    }
+
+    return response
+      .status(200)
+      .send({ message: "Post updated successfully" });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
 // DELETE
 // Delete One
 router.delete("/deleteOne/:comment_id", async (request, response) => {
