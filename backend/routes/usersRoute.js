1732ef56c81aa8c44d36a59c80bdb73802642c06
Roberto J Rodriguez <robertojrodriguez21@gmail.com>
2024-09-30 22:01:05 -0500

    Created removeBookmark call to usersRoute.

--- backend/routes/usersRoute.js
+++ backend/routes/usersRoute.js
@@ -280,6 +280,26 @@ router.put("/addBookmark/:user_id/:post_id", async (request, response) => {
   }
 });
 
+// Update One - remove one bookmark from one user
+router.put("/removeBookmark/:user_id/:post_id", async (request, response) => {
+  try {
+    const { user_id, post_id } = request.params;
+
+    const result = await User.findByIdAndUpdate(user_id, { $pull: { bookmarks: post_id } });
+
+    if (!result) {
+      return response.status(404).json({ message: "User not found" });
+    }
+
+    return response
+      .status(200)
+      .send({ message: "User updated successfully" });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
 // DELETE
 // Delete One
 router.delete("/:user_id", async (request, response) => {
