9a3d9f7ec5f9582395da18b6dd9d0348144be425
Roberto J Rodriguez <robertojrodriguez21@gmail.com>
2024-09-30 23:30:00 -0500

    Created addComment call to usersRoute.

--- backend/routes/usersRoute.js
+++ backend/routes/usersRoute.js
@@ -380,6 +380,26 @@ router.put("/removePost/:user_id/:post_id", async (request, response) => {
   }
 });
 
+// Update One - add one comment to one user
+router.put("/addComment/:user_id/:comment_id", async (request, response) => {
+  try {
+    const { user_id, comment_id } = request.params;
+
+    const result = await User.findByIdAndUpdate(user_id, { $addToSet: { comments: comment_id } });
+
+    if (!result) {
+      return response.status(404).json({ message: "User not found" });
+    }
+
+    return response
+      .status(200)
+      .send({ message: "User updated successfully" });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
 // DELETE
 // Delete One
 router.delete("/deleteOne/:user_id", async (request, response) => {
